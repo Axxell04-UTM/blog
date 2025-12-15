39c9f441fb9436f4c8d2a2db151f76e553b4b939
Axxell04-UTM <alajones1404@utm.edu.ec>
2025-12-15 14:33:04 -0500

    fix: enforce SSL connection for PostgreSQL client in db index

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,6 +5,8 @@ import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = postgres(env.DATABASE_URL);
+const client = postgres(env.DATABASE_URL, {
+    ssl: 'require'
+});
 
 export const db = drizzle(client, { schema });
