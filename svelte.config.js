d437d2754c1c46d57032a1cc40c297a6df0f05fe
Axxell04-UTM <alajones1404@utm.edu.ec>
2026-01-11 19:30:09 -0500

    feat: update SvelteKit configuration to conditionally use Vercel or Node adapter based on environment

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
 // import adapter from '@sveltejs/adapter-node';
-import adapter from '@sveltejs/adapter-vercel';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const isVercel = process.env.VERCEL === '1';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +15,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: isVercel
+			? adapterVercel()
+			: adapterNode({
+				out: 'build'
+			})
 	}
 };
 
